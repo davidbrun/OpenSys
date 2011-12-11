4373e908cb103ce1ebb0b4d86c584c6902bd6cf2
Michael Muré <batolettre@gmail.com>
2011-12-11 17:30:27 +0100

    ajout de logger d'entree/sortie

---
+++ src/fr/uha/ensisa/opensys/IO/LoggerInput.java
@@ -0,0 +1,40 @@
+package fr.uha.ensisa.opensys.IO;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import fr.uha.ensisa.opensys.core.Input;
+import fr.uha.ensisa.opensys.core.OpenSys;
+
+public class LoggerInput extends Input {
+
+	static {
+		OpenSys.addInput(LoggerInput.class.getName().toLowerCase(), LoggerInput.class);
+	}
+	
+	private PrintWriter out;
+
+	public LoggerInput(String filename) throws IOException {
+		out = new PrintWriter(new FileWriter(filename));
+	}
+	
+	public void filnalize() {
+		out.close();
+	}
+	
+	@Override
+	public int getChar() {
+		int i = ((Input)next).getChar();
+		out.print(i);
+		return i;
+	}
+
+	@Override
+	public String getLine() {
+		String s = ((Input)next).getLine();
+		out.println(s);
+		return s;
+	}
+
+}
---
+++ src/fr/uha/ensisa/opensys/IO/LoggerOutput.java
@@ -0,0 +1,38 @@
+package fr.uha.ensisa.opensys.IO;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import fr.uha.ensisa.opensys.core.OpenSys;
+import fr.uha.ensisa.opensys.core.Output;
+
+public class LoggerOutput extends Output {
+
+	static {
+		OpenSys.addOutput(LoggerOutput.class.getName().toLowerCase(), LoggerOutput.class);
+	}
+	
+	private PrintWriter out;
+
+	public LoggerOutput(String filename) throws IOException {
+		out = new PrintWriter(new FileWriter(filename));
+	}
+	
+	public void filnalize() {
+		out.close();
+	}
+	
+	@Override
+	public void printChar(char c) {
+		out.print(c);
+		((Output)next).printChar(c);
+	}
+
+	@Override
+	public void printLine(String s) {
+		out.println(s);
+		((Output)next).printLine(s);
+	}
+
+}
