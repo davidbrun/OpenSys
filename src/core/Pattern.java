cb3eb0f233195da1d34d3ba8842a9ea9ff0eb160
Michael Muré <batolettre@gmail.com>
2011-12-11 14:03:46 +0100

    ajout structure de base

---
+++ src/core/Pattern.java
@@ -0,0 +1,16 @@
+package core;
+
+public abstract class Pattern {
+	private Pattern next = null;
+
+	public Pattern getNext() {
+		return next;
+	}
+
+	public void setNext(Pattern next) {
+		this.next = next;
+	}
+	
+	public abstract void execute(ICommand cmd);
+
+}
